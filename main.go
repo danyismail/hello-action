8cb8bc7bb55753c7ce482fef7542b08ad6b824f5
dany ismail <dany.perdana@smartfren.com>
2024-03-05 13:32:15 +0700

    test trigger

--- main.go
+++ main.go
@@ -7,17 +7,12 @@ import (
 )
 
 func main() {
-	// Create a new Echo instance
 	e := echo.New()
 	text := `
         Hello
     `
-
-	// Define routes
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, text)
 	})
-
-	// Start the server
 	e.Start(":1234")
 }
